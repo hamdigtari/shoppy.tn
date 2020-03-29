e71d12509f290dbd1bd07dd68a7c61649e4c9e73
anasfattoum <mohamedanas.fattoum@esprit.tn>
2020-03-29 13:30:40 +0100

    Creation class category

---
+++ src/tn/shoppy/model/Category.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.shoppy.model;
+
+/**
+ *
+ * @author anas fattoum
+ */
+public class Category {
+    private int id;
+    private String nom;
+    
+    
+    public Category() {
+        this(0,"name");
+    }
+
+    public Category(int id,String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "id=" + id + ", nom=" + nom + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+
+    
+}
